e78077355d55351d218a53d98d8d0e145ab1fa5d
shahar-y <shya95@gmail.com>
2021-01-06 19:29:55 +0200

    part1+part2
    Signed-off-by: shahar-y <shya95@gmail.com>

---
+++ rabbitmq/part1/send/send.go
@@ -0,0 +1,53 @@
+package main
+
+// go run send/send.go
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+const (
+	rabbitURL = "amqp://guest:guest@localhost:5672/"
+)
+
+func main() {
+	fmt.Printf("starting...\n")
+	conn, err := amqp.Dial(rabbitURL)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
